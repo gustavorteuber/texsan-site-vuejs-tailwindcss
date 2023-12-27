bfe468ab74687f1240c42545cf83425a845fb1b1
gustavorteuber <gugareis135@gmail.com>
2023-12-27 15:31:32 -0300

    modificacoes e texto

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -24,59 +24,39 @@ export default {
 <template>
   <nav :class="colorFooter()">
     <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-      <img class="w-20" src="../assets/texsantransparent.png" alt="" />
-      <ul
-        class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12"
-      >
+      <a href="/">
+        <img class="w-20" src="../assets/texsantransparent.png" alt="" />
+      </a>
+      <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
         <li><a class="hover:text-gray-500" href="/">Home</a></li>
         <li>
-          <RouterLink class="hover:text-gray-500" to="/sobrenos"
-            >Sobre nós
+          <RouterLink class="hover:text-gray-500" to="/sobrenos">Sobre nós
           </RouterLink>
         </li>
         <li>
-          <a
-            class="hover:text-gray-5 00"
-            href="mailto:texsan@texsan.com.br"
-            target="_blank"
-            >Entre em contato</a
-          >
+          <a class="hover:text-gray-5 00" href="mailto:texsan@texsan.com.br" target="_blank">Entre em contato</a>
         </li>
       </ul>
-      <div class="hidden xl:flex items-center space-x-5 items-center">
-        <button
-          v-if="isDark == false"
-          type="button"
-          @click="isDark = !isDark"
-          class="block py-2 pl-3 pr-4 text-neutral-700 rounded hover:bg-neutral-100 md:hover:bg-transparent md:hover:text-yellow-700 md:p-0 :text-neutral-400 md::hover:text-yellow-500 :hover:bg-neutral-700 :hover:text-gray-500 md::hover:bg-transparent :border-cyan-700"
-        >
+      <div class="hidden xl:flex items-center space-x-5">
+        <button v-if="isDark == false" type="button" @click="isDark = !isDark"
+          class="block py-2 pl-3 pr-4 text-neutral-700 rounded hover:bg-neutral-100 md:hover:bg-transparent md:hover:text-yellow-700 md:p-0 :text-neutral-400 md::hover:text-yellow-500 :hover:bg-neutral-700 :hover:text-gray-500 md::hover:bg-transparent :border-cyan-700">
           <i class="fa-regular fa-sun"></i>
         </button>
-        <button
-          v-if="isDark == true"
-          type="button"
-          @click="isDark = !isDark"
-          class="block py-2 pl-3 pr-4 text-neutral-700 rounded hover:bg-neutral-100 md:hover:bg-transparent md:hover:text-cyan-700 md:p-0 :text-neutral-400 md::hover:text-cyan-500 :hover:bg-cyan-700 :hover:text-emerald-500 md::hover:bg-transparent :border-neutral-700"
-        >
+        <button v-if="isDark == true" type="button" @click="isDark = !isDark"
+          class="block py-2 pl-3 pr-4 text-neutral-700 rounded hover:bg-neutral-100 md:hover:bg-transparent md:hover:text-cyan-700 md:p-0 :text-neutral-400 md::hover:text-cyan-500 :hover:bg-cyan-700 :hover:text-emerald-500 md::hover:bg-transparent :border-neutral-700">
           <i class="fa-regular fa-moon"></i>
         </button>
       </div>
     </div>
     <a class="xl:hidden flex mr-6 items-center" href="#">
-      <button
-        v-if="isDark == false"
-        type="button"
+      <button v-if="isDark == false" type="button"
         class="block py-2 pl-3 pr-4 text-yellow-400 rounded md:hover:bg-transparent md:hover:text-cyan-700 md:p-0 :text-neutral-400 md::hover:text-cyan-500 :hover:bg-cyan-700 :hover:text-emerald-500 md::hover:bg-transparent :border-neutral-700"
-        @click="isDark = !isDark"
-      >
+        @click="isDark = !isDark">
         <i class="fa-regular fa-sun"></i>
       </button>
-      <button
-        v-if="isDark == true"
-        type="button"
+      <button v-if="isDark == true" type="button"
         class="block py-2 pl-3 pr-4 text-cyan-700 rounded md:hover:bg-transparent md:hover:text-cyan-700 md:p-0 :text-neutral-400 md::hover:text-cyan-500 :hover:bg-neutral-700 :hover:text-gray-500 md::hover:bg-transparent :border-cyan-700"
-        @click="isDark = !isDark"
-      >
+        @click="isDark = !isDark">
         <i class="fa-regular fa-moon"></i>
       </button>
     </a>
--- src/components/HeadingComp.vue
+++ src/components/HeadingComp.vue
@@ -12,9 +12,9 @@ export default {
   methods: {
     colorText() {
       if (this.isDark == false) {
-        return "mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-text-4xl lg:text-7xl transition delay-150 duration-300 ease-in-out";
+        return "mb-5 text-6xl font-extrabold leading-none tracking-tight text-white md:text-text-4xl lg:text-7xl transition delay-150 duration-300 ease-in-out";
       } else {
-        return "mb-4 text-4xl font-extrabold text-slate-800 md:text-text-4xl lg:text-7xl transition delay-150 duration-300 ease-in-out";
+        return "mb-5 text-6xl font-extrabold text-slate-800 md:text-text-4xl lg:text-7xl transition delay-150 duration-300 ease-in-out";
       }
     },
     colorButton() {
@@ -32,8 +32,7 @@ export default {
   <div class="all">
     <div class="flex justify-center">
       <h1
-        class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-transparent bg-clip-text bg-gradient-to-l from-emerald-700 via-cyan-800 to-indigo-900 md:text-text-4xl lg:text-7xl"
-      >
+        class="mb-4 text-7xl font-extrabold leading-none tracking-tight text-transparent bg-clip-text bg-gradient-to-l from-emerald-700 via-cyan-800 to-indigo-900 md:text-text-4xl lg:text-7xl">
         O site da Texsan
         <div class="flex justify-center">
           <h1 :class="colorText()">está chegando</h1>
@@ -41,30 +40,22 @@ export default {
       </h1>
     </div>
     <div class="flex justify-center">
-      <p
-        class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48"
-      >
-        Em breve o site da texsan estará no ar!
+      <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+        A Texsan Saneamentos destaca-se como referência no mercado de fornecimento de materiais para manutenções
+        hidráulicas, oferecendo produtos de alta qualidade que atendem às demandas mais exigentes. Nosso compromisso com a
+        excelência reflete-se não apenas na durabilidade e eficiência de nossos produtos, mas também na agilidade
+        excepcional de nossas entregas. Priorizamos a satisfação do cliente, proporcionando um serviço eficaz e pontual,
+        garantindo que nossos clientes recebam seus materiais no prazo estabelecido. Conte conosco para suprir suas
+        necessidades hidráulicas com confiança e eficiência, pois na Texsan, a qualidade e a rapidez caminham juntas.
       </p>
     </div>
     <div class="flex justify-center">
-      <a
-        href="https://wa.me/5547992877667"
-        target="_blank"
-        :class="colorButton()"
-      >
+      <a href="https://wa.me/5547992877667" target="_blank" :class="colorButton()">
         Saiba mais
-        <svg
-          class="w-5 h-5 ml-2 -mr-1"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
+        <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd"
             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
+            clip-rule="evenodd"></path>
         </svg>
       </a>
     </div>
